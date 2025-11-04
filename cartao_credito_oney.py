929b5418709b70765ad16bc1481d41d24c541483
DiogoMMP <diogom.pereira@hotmail.com>
2025-11-04 15:40:32 +0000

    Adição da possibilidade de fazer tudo o que faziamos com o cartão de crédito Santander e Universo mas agora também com o do Oney.

---
+++ cartao_credito_oney.py
@@ -0,0 +1,135 @@
+from notion_client import Client
+from datetime import datetime, timezone
+import os
+
+# === CONFIGURAÇÃO DO NOTION ===
+NOTION_TOKEN = os.environ["NOTION_TOKEN"]
+
+# === BASES DE DADOS ===
+ENTRADAS_BD = "23cfdc291a7281fc8b9dc4cbbf617ed0"
+SAIDAS_BD = "23cfdc291a7281be8c7ed8b65b76da7e"
+PAGAR_CARTAO_CREDITO = "2a1fdc291a7280dcb7e2cd8a1bb468ae"
+
+# === IDs DAS CONTAS ===
+CARTAO_ONEY = "2a1fdc291a72807daabaf0e43515ce78"
+
+# === IDs DAS CATEGORIAS ===
+DESPESAS_MENSAIS_CATEGORIA = "23efdc29-1a72-80e5-bb24-d9b646ddb21b"
+
+# === NOMES DAS PROPRIEDADES ===
+P_NOME = "Nome"                 #Texto
+P_CATEGORIA = "Categoria"       #Relação
+P_CONTA = "Conta"               #Relação
+P_DATA = "Data"                 #Data
+P_VALOR = "Valor"               #Número
+
+# === CLIENTE NOTION ===
+notion = Client(auth=NOTION_TOKEN)
+
+# === OBTER VALORES DAS ENTRADAS ===
+config_page = notion.pages.retrieve(PAGAR_CARTAO_CREDITO)
+
+inicio = config_page["properties"]["Início"]["date"]["start"]
+fim = config_page["properties"]["Fim"]["date"]["start"]
+pago = config_page["properties"]["Pago"]["checkbox"]
+valor = config_page["properties"]["Valor"]["number"]
+
+if pago is True:
+    # Entradas
+    results = notion.databases.query(
+        **{
+            "database_id": ENTRADAS_BD,
+            "filter": {
+                "and": [
+                    {
+                        "property": "Conta",
+                        "relation": {
+                            "contains": CARTAO_ONEY
+                        }
+                    },
+                    {
+                        "property": "Data",
+                        "date": {
+                            "on_or_after": inicio
+                        }
+                    },
+                    {
+                        "property": "Data",
+                        "date": {
+                            "before": fim
+                        }
+                    },
+                    {
+                        "property": "Saiu",
+                        "checkbox": {
+                            "equals": False
+                        }
+                    }
+                ]
+            }
+        }
+    )["results"]
+
+    for result in results:
+        page_id = result["id"]
+        notion.pages.update(
+            page_id,
+            properties={
+                "Saiu": {"checkbox": True}
+            }
+        )
+
+    hoje = datetime.now(timezone.utc)
+
+    notion.pages.create(
+        parent={"database_id": SAIDAS_BD},
+        properties={
+            P_NOME: {
+                "title": [
+                    {"text": {"content": "Pagamento do Cartão de Crédito Oney"}}
+                ]
+            },
+            P_CATEGORIA: {
+                "relation": [
+                    {"id": DESPESAS_MENSAIS_CATEGORIA}
+                ]
+            },
+            P_CONTA: {
+                "relation": [
+                    {"id": CARTAO_ONEY}
+                ]
+            },
+            P_DATA: {
+                "date": {
+                    "start": hoje.isoformat()
+                }
+            },
+            P_VALOR: {
+                "number": valor
+            }
+        }
+    )
+
+    data_15 = hoje.replace(day=15)
+
+    notion.pages.update(
+        PAGAR_CARTAO_CREDITO,
+        properties={
+            "Valor": {
+                "number": 0
+            },
+            "Pago":{
+                "checkbox": False
+            },
+            "Início":{
+                "date": {
+                    "start": fim.split("T")[0]
+                }
+            },
+            "Fim":{
+                "date": {
+                    "start": data_15.date().isoformat()
+                }
+            }
+        }
+    )
